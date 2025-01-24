43fdd63865e03ac61ed83f686e4b1fbe857633d5
sajib-isharify <sajib.isharify2@gmail.com>
2025-01-24 11:19:39 +0600

    structure initislized

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar"
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Navbar />
     </>
   );
 }
